fd29f2ee38717c5a841f332bb23a82d32d59fa6b
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-14 22:12:03 +0300

    доделал задание

--- Number10/Program.cs
+++ Number10/Program.cs
@@ -4,10 +4,17 @@
 // 782 -> 8
 // 918 -> 1
 
-
+//С рандомным числом
+/*
 Console.WriteLine();
 int number = new Random().Next(100, 1000);
 
 Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
 
 Console.WriteLine(); 
+*/
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+
