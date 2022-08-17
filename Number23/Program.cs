880dec2f984ca0f418f5892ab9cfa624495eb675
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-17 15:59:11 +0300

    Number23

---
+++ Number23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int cube = 1;
+while (cube <= number)
+{
+    int result = cube * cube * cube;
+    Console.Write(" " + result);
+    cube++;
+}
