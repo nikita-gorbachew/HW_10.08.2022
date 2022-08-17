adc1dbe4224064ef41752f3a38f69d9fe63c4e31
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-17 17:55:13 +0300

    Number29

---
+++ Number29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Массив от 0 до N. Введите N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+for (int i = 0; i < number; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+Console.WriteLine(($"[{String.Join(", ", array)}]"));
