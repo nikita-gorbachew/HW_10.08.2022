7f0d3c31300e764b1a290f4f86526390381d2884
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-17 17:05:05 +0300

    Number27

---
+++ Number27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = 0;
+while (number != 0)
+{
+    digit += number % 10;
+    number /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе = {digit}");
