2cf603a3a5c6c4fb896f9293a54b78cd5acb97b1
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-23 14:30:45 +0300

    Number34

---
+++ Number34/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+Console.Write("Массив от 0 до N. Введите N: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(size, 100, 999);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0) count++;
+}
+Console.WriteLine("Четных чисел в массиве: " + count);
