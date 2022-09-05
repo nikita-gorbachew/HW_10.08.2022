4a48379fe8f5d41975f4280d3ae7fb14f4f82bc5
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-09-06 01:26:18 +0300

    Number66

---
+++ Number66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < M) return;
+int SumInterval(int start, int end)
+{
+    if(start > end) return 0;
+    return start + SumInterval(start + 1, end);
+}
+Console.WriteLine(SumInterval(M, N));
